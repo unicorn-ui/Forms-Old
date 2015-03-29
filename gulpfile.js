7baa0b6596a4515c259f45af6ca9509b38d8a54b
Rob Levin <roblevintennis@gmail.com>
2015-03-29 08:41:08 -0700

    -mAdded some gulp

---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+var gulp            = require('gulp');
+var exec            = require('child_process').exec;
+var sass            = require('gulp-sass');
+var autoprefixer    = require('gulp-autoprefixer');
+var mainBowerFiles  = require('main-bower-files');
+var usemin          = require('gulp-usemin');
+var minifyCSS       = require('gulp-minify-css');
+var concat          = require('gulp-concat');
+var webpack         = require('gulp-webpack');
+var uglify          = require('gulp-uglify');
+var rev             = require('gulp-rev');
+var imageResize     = require('gulp-image-resize');
+var connect         = require('gulp-connect');
+var Q               = require('q');
+var del             = require('del');
+var rename          = require("gulp-rename");
+var openPage        = require("gulp-open");
+var bower           = require("bower");
+var flatten         = require('gulp-flatten');
+
+/**
+*  CLEAN
+*
+*  Delete files and directories
+*/
+
+gulp.task('clean:css', function() {
+  return Q.promise(function(resolve, error) {
+    del(['css'], resolve);
+  });
+});
+
+
+/**
+*  MOVE JS FILES
+*
+*/
+gulp.task('moveJSLibs', [], function(){
+  var stream = gulp.src([
+        'vendor/js/**/*.js'
+      ])
+      .pipe(flatten())
+      .pipe(uglify())
+      .pipe(gulp.dest('js'));
+
+  return stream;
+});
+
+gulp.task('moveCSSLibs', ['clean:css'], function(){
+  var stream = gulp.src([
+      'vendor/css/**/*.css'
+    ])
+    .pipe(concat('vendor.min.css'))
+    .pipe(minifyCSS({ noAdvanced: true }))
+    .pipe(gulp.dest('css'));
+
+  return stream;
+});
+
+/**
+*  CSS PREPROCESSING
+*
+*  Sass, vender prefix, minify, move
+*/
+gulp.task('css', ['clean:css'], function() {
+  var stream = gulp.src('scss/**/*.scss')
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(minifyCSS({ noAdvanced: true }))
+      .pipe(gulp.dest('css'));
+
+  return stream;
+});
+
+/**
+*  CONNECT SERVER
+*
+*  Loads the server locally and reloads when
+*  connect.reload() is called.
+*/
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    port: 8000,
+    livereload: true
+  });
+});
+
+
+/**
+*  BUILD TASKS
+*
+*  Local and production build tasks
+*/
+
+gulp.task('default', ['css', 'moveJSLibs', 'moveCSSLibs', 'connect'], function() {
+
+  //OPEN IN BROWSER
+  var stream = gulp.src("./index.html")
+      .pipe(openPage("", {
+        app: "Google Chrome",
+        url: "http://localhost:8000"
+      }));
+
+  return stream;
+});
+
+
+
